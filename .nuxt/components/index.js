f08849ae31935c69baebbac183f172625b2bde8e
L. Palharês Rosa <luciano.palhares@enesolucoes.com.br>
2025-01-20 21:29:13 -0300

    Remove .nuxt from the repository

--- .nuxt/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Logo } from '../../components/Logo.vue'
-export { default as UserForm } from '../../components/UserForm.vue'
-export { default as Loading } from '../../components/loading.vue'
-
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
-export const LazyUserForm = import('../../components/UserForm.vue' /* webpackChunkName: "components/UserForm" */).then(c => c.default || c)
-export const LazyLoading = import('../../components/loading.vue' /* webpackChunkName: "components/loading" */).then(c => c.default || c)
--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['logged'] = require('../middleware/logged.js')
-middleware['logged'] = middleware['logged'].default || middleware['logged']
-
-export default middleware
